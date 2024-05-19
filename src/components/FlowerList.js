bf2675d73df140693332f63e333a7ad657146a77
Christin Göb <christin.goeb@t-online.de>
2024-05-19 22:40:40 +0200

    search bar functionality

--- src/components/FlowerList.js
+++ src/components/FlowerList.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import FlowerCard from './FlowerCard';
+import SearchBar from './SearchBar';
 
 function FLowerList({ flower }) {
+  const [filteredFlowers, setFilteredFlowers] = useState(flower);
+
+  const handleSearch = (query) => {
+    const lowerCaseQuery = query.toLowerCase();
+    const filtered = flower.filter((flower) =>
+      flower.name.toLowerCase().includes(lowerCaseQuery) ||
+      flower.description.toLowerCase().includes(lowerCaseQuery)
+    );
+    setFilteredFlowers(filtered);
+  };
+    useEffect(() => {
+      setFilteredFlowers(flower);
+    }, [flower]);
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      {flower.map(flower => (
+      <SearchBar onSearch={handleSearch} />
+      {filteredFlowers.map((flower) => (
         <Box key={flower.id} mb={2} width="100%">
           <FlowerCard flower={flower} />
         </Box>
