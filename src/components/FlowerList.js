cbe8c19120e21f84ecdba232b8a87d5eb4c07e68
Tina Wahmkow <tina.wahmkow@stud.htwk-leipzig.de>
2024-08-01 11:17:31 +0200

    search works now

--- src/components/FlowerList.js
+++ src/components/FlowerList.js
@@ -1,36 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import FlowerCard from "./FlowerCard";
-import SearchBar from "./SearchBar";
 
 function FlowerList({ flowerData }) {
   const [filteredFlowers, setFilteredFlowers] = useState(flowerData);
 
-  const handleSearch = (query) => {
-    const lowerCaseQuery = query.toLowerCase();
-    const filtered = flowerData.filter(
-      (flowerData) =>
-        flowerData.name.toLowerCase().includes(lowerCaseQuery) ||
-        flowerData.description.toLowerCase().includes(lowerCaseQuery)
-    );
-    setFilteredFlowers(filtered);
-  };
   useEffect(() => {
     setFilteredFlowers(flowerData);
   }, [flowerData]);
   return (
     <Typography variant="h4" component="h1" gutterBottom>
       <Box display="flex" flexDirection="column" alignItems="center">
-        <SearchBar onSearch={handleSearch} />
-        {filteredFlowers.length > 0 ? ( // Conditional rendering
-          filteredFlowers.map((flowerData) => (
-            <Box key={flowerData.id} mb={2} width="100%">
-              <FlowerCard flower={flowerData} />
-            </Box>
-          ))
-        ) : (
-          <p>Keine Blumen gefunden. :c</p> // Message when no flowers are available
-        )}
+        {filteredFlowers.map((flowerData) => (
+          <Box key={flowerData.id} mb={2} width="100%">
+            <FlowerCard flower={flowerData} />
+          </Box>
+        ))}
       </Box>
     </Typography>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Container, Typography, CircularProgress } from "@mui/material";
+import { Container, Typography, CircularProgress, Box, OutlinedInput, InputLabel, MenuItem, FormControl, Select, Button, TextField } from "@mui/material";
 import FlowerList from "../components/FlowerList";
 import axios from "axios";
-
-/* import axiosWrapper from '../axios';
- */import { api_base_url } from '../settings.json';
+import { api_base_url } from '../settings.json';
 
 function Home({ flowerData }) {
   const [flower, setFlower] = useState([]);
   const [loading, setLoading] = useState([]);
+  const [selectedColors, setColors] = useState([]);
+  const [searchTerms, setSearchTerms] = useState("");
+
+  const names = [
+    "pink",
+    "red",
+    "green",
+    "yellow",
+    "purple",
+    "white",
+    "blue",
+    "orange"
+  ];
 
   useEffect(() => {
     getFilteredFlowers(); // default gets all flowers with no set filter
@@ -25,27 +36,92 @@ function Home({ flowerData }) {
         indexes: null,
       }
     }).then(response => {
-      console.log(response.data)
       setFlower(response.data);
       setLoading(false)
     }).catch(e => {
-      console.log(e)
+      console.error(e)
       setLoading(false)
     })
   }
 
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 200,
+      },
+    },
+  };
+
+
+  const handleChange = (event) => {
+    const { target: { value } } = event;
+    setColors(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
   return (
-    <Container>
+    <Container sx={{ p: 2 }}>
+      <Typography variant="h4" component="h1" gutterBottom sx={{ display: 'flex', alignItems: 'center', justifyContent: "center" }}>
+        Stell dir deinen ganz eigenen Blumenstrauß zusammen
+      </Typography>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center", "marginBottom": "1rem" }}>
+        <TextField
+          label="Stichworte"
+          variant="outlined"
+          placeholder="Nach Blumen suchen"
+          value={searchTerms}
+          onChange={(e) => { setSearchTerms(e.target.value) }}
+          sx={{ mr: 2, width: 450 }}
+        />
+        <FormControl sx={{ mr: 2, width: 200 }}>
+          <InputLabel id="color-input">Farbfilter</InputLabel>
+          <Select
+            labelId="color-input-label"
+            id="color-input"
+            multiple
+            autoWidth
+            value={selectedColors}
+            onChange={handleChange}
+            input={<OutlinedInput id="select-multiple-chip" label="Farbfilter" />}
+            renderValue={(selected) => (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                {selected.map((value) => (
+                  <div key={value} style={{ borderRadius: "15px", background: "rgba(0, 0, 0, 0.08)", padding: "0.5rem", display: 'flex', alignItems: 'center' }}>
+                    <div style={{ background: value, width: 15, height: 15, borderRadius: "50%", marginRight: "0.5rem", border: "1px black solid" }}></div>
+                    <div>{value}</div>
+                  </div>
+
+                ))}
+              </Box>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+              >
+                <div style={{ background: name, width: 30, height: 30, borderRadius: "50%", marginRight: "1rem", border: "1px black solid" }}></div>
+                <div>{name}</div>
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button variant="contained" onClick={() => getFilteredFlowers(selectedColors, searchTerms.split(" "))}>Filter anwenden</Button>
+      </div>
       {(!flower && !loading) ?
-        <p>No flower data available</p>
-        : <>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Stell dir deinen ganz eigenen Blumenstrauß zusammen
-          </Typography>
+        <p>Keine Blumen gefunden :c</p>
+        :
+        <>
+          {loading && <CircularProgress />}
           <FlowerList flowerData={flower} />
-        </>}
-        { loading && <CircularProgress />}
+        </>
+      }
     </Container>
   );
 }
